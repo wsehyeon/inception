89224e42fa5dd5eea01df8ddd452d409e896e45d
swang <swang@student.42seoul.kr>
2022-09-28 22:34:29 +0900

    fix env typo

--- srcs/requirements/mariadb/tools/mariadb-setting.sh
+++ srcs/requirements/mariadb/tools/mariadb-setting.sh
@@ -1,10 +1,10 @@
 service mysql start;
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME";
-mysql -e "CREATE USER IF NOT EXISTS '$WP_ADMIN_USER'@'%' IDENTIFIED BY '$WP_ADMIN_PASSWORD'";
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'";
-mysql $DB_DATABASE -u root < ./wp_backup.sql
-mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT'; FLUSH PRIVILEGES;"
+mysql -e "CREATE USER IF NOT EXISTS '$WP_ADMIN_USER'@'%' IDENTIFIED BY '$WP_ADMIN_PASS'";
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_ADMIN_USER'@'%'";
+mysql $DB_NAME -u root < ./wp_backup.sql
+mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASS'; FLUSH PRIVILEGES;"
 mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASS shutdown
 
 exec mysqld
